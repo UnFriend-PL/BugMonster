47910db17ddb0178ff9e4454990a47e01906e739
DominikRog <rogowskidominik331@gmail.com>
2023-02-04 22:19:05 +0100

    działał mining ale juz nie działa

--- Roots/Assets/Scripts/IsMining.cs
+++ Roots/Assets/Scripts/IsMining.cs
@@ -9,17 +9,5 @@ public class IsMining : MonoBehaviour
     void OnCollisionEnter2D(Collision2D coll)
     {
         coll.gameObject.GetComponent<Block>().Mine();
-
-        Vector2 position = Vector2.zero;
-        var colliders = Physics.OverlapBox(position, Vector3.one * 1.5f);
-
-
-        foreach (Collider col in colliders)
-        {
-            if (col.TryGetComponent<Block>(out var tile))
-            {
-               tile.isFoggy = false; 
-            }
-        }
     }
 }
