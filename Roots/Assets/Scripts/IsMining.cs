10452e167f8fc0e76734c2c66ebc98eef672c603
DominikRog <rogowskidominik331@gmail.com>
2023-02-04 21:02:10 +0100

    dalej is maining

--- Roots/Assets/Scripts/IsMining.cs
+++ Roots/Assets/Scripts/IsMining.cs
@@ -8,7 +8,18 @@ public class IsMining : MonoBehaviour
     
     void OnCollisionEnter2D(Collision2D coll)
     {
-        //GetObject<Block>().type = blockType.Background;
-        //Debug.Log(transform.position);
+        coll.gameObject.GetComponent<Block>().Mine();
+
+        Vector2 position = Vector2.zero;
+        var colliders = Physics.OverlapBox(position, Vector3.one * 1.5f);
+
+
+        foreach (Collider col in colliders)
+        {
+            if (col.TryGetComponent<Block>(out var tile))
+            {
+               tile.isFoggy = false; 
+            }
+        }
     }
 }
