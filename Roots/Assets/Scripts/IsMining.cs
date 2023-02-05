8125b68f574d8da80f5a546a456f0ce4b2b96647
dimicw <104204285+dimicw@users.noreply.github.com>
2023-02-05 01:36:25 +0100

    Ant Eats Dirt!!!!!

--- Roots/Assets/Scripts/IsMining.cs
+++ Roots/Assets/Scripts/IsMining.cs
@@ -8,6 +8,7 @@ public class IsMining : MonoBehaviour
     
     void OnCollisionEnter2D(Collision2D coll)
     {
+        Debug.Log("collision!");
         coll.gameObject.GetComponent<Block>().Mine();
     }
 }
