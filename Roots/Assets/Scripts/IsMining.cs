870d872557c709b4dc396a9fe2dd66b4ee50a050
UnFriend-PL <104201558+UnFriend-PL@users.noreply.github.com>
2023-02-05 13:58:31 +0100

    isMining commented

--- Roots/Assets/Scripts/IsMining.cs
+++ Roots/Assets/Scripts/IsMining.cs
@@ -12,50 +12,52 @@ public class IsMining : MonoBehaviour
 
     void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject.tag == MineTag) {
-            if (collision.gameObject.GetComponent<Block>().Mine())
-                {
-                    value.GetComponent<ScoreManager>().countOfDirt++;
-                    value.GetComponent<ScoreManager>().Start();
-                }
-        }
-    
-    void OnCollisionEnter2D(Collision2D coll)
-    {
-        Debug.Log($"collision! with {coll.gameObject.tag}");
-        if (coll.gameObject.tag == "Block" || coll.gameObject.tag == "Food")
+        if (collision.gameObject.tag == MineTag)
         {
-            coll.gameObject.GetComponent<Block>().Mine();
+            if (collision.gameObject.GetComponent<Block>().Mine())
+            {
+                value.GetComponent<ScoreManager>().countOfDirt++;
+                value.GetComponent<ScoreManager>().Start();
+            }
         }
-        //if(coll.gameObject.tag == "Bug")
+
+        //void OnCollisionEnter2D(Collision2D coll)
         //{
-        //    //coll.gameObject.GetComponent<SelectAnt>().HP--;
-        //    //var bugHp = coll.gameObject.GetComponent<Bug>().HP;
-        //    //coll.gameObject.GetComponent<SelectAnt>().HP -= 1;
-        //    //if(coll.gameObject.GetComponent<SelectAnt>().HP <= 0)
-        //    //{
-        //    SelectAnt selectAnt = GetComponent<SelectAnt>();
-        //    if (selectAnt != null)
+        //    Debug.Log($"collision! with {coll.gameObject.tag}");
+        //    if (coll.gameObject.tag == "Block" || coll.gameObject.tag == "Food")
         //    {
-        //        Debug.Log("Ant should be destroyed");
-        //        Destroy(gameObject);
-        //        //MonoBehaviour[] scripts = coll.gameObject.GetComponents<MonoBehaviour>();
-        //        //foreach (MonoBehaviour script in scripts)
-        //        //{
-        //        //    DestroyImmediate(script);
-        //        //}
-        //        //DestroyImmediate(coll.gameObject);
+        //        coll.gameObject.GetComponent<Block>().Mine();
         //    }
-        //    //    }
-        //    //    if(coll.gameObject.GetComponent<Bug>().HP > 0)
-        //    //    {
-        //    //        coll.gameObject.GetComponent<Bug>().HP -= 1;
-        //    //    }
-        //    //    else if(coll.gameObject.GetComponent<Bug>().HP <= 0)
-        //    //    {
-        //    //        Destroy(coll.gameObject.GetComponent<Bug>());
-        //    //    }
-        //    //    Debug.Log($"{coll.gameObject.GetComponent<SelectAnt>().HP} - ANT HP");
+            //if(coll.gameObject.tag == "Bug")
+            //{
+            //    //coll.gameObject.GetComponent<SelectAnt>().HP--;
+            //    //var bugHp = coll.gameObject.GetComponent<Bug>().HP;
+            //    //coll.gameObject.GetComponent<SelectAnt>().HP -= 1;
+            //    //if(coll.gameObject.GetComponent<SelectAnt>().HP <= 0)
+            //    //{
+            //    SelectAnt selectAnt = GetComponent<SelectAnt>();
+            //    if (selectAnt != null)
+            //    {
+            //        Debug.Log("Ant should be destroyed");
+            //        Destroy(gameObject);
+            //        //MonoBehaviour[] scripts = coll.gameObject.GetComponents<MonoBehaviour>();
+            //        //foreach (MonoBehaviour script in scripts)
+            //        //{
+            //        //    DestroyImmediate(script);
+            //        //}
+            //        //DestroyImmediate(coll.gameObject);
+            //    }
+            //    //    }
+            //    //    if(coll.gameObject.GetComponent<Bug>().HP > 0)
+            //    //    {
+            //    //        coll.gameObject.GetComponent<Bug>().HP -= 1;
+            //    //    }
+            //    //    else if(coll.gameObject.GetComponent<Bug>().HP <= 0)
+            //    //    {
+            //    //        Destroy(coll.gameObject.GetComponent<Bug>());
+            //    //    }
+            //    //    Debug.Log($"{coll.gameObject.GetComponent<SelectAnt>().HP} - ANT HP");
+            //}
         //}
     }
 }
